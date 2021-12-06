782d921d7ea7955089f20ba40ac1a46a83151775
nitish1162 <nitishkc789@gmail.com>
2021-12-06 09:29:26 +0530

    Addig usecase1

---
+++ src/gambler/GamblerSimulator.java
@@ -0,0 +1,18 @@
+package gambler;
+
+public class GamblerSimulator {
+	int dailyStack = 100;
+    int win   = 0;
+    int loose = 0;
+	
+	public static void main(String[] args) {
+	
+		GamblerLimit();
+		
+	}
+	
+	public static int GamblerLimit() {
+		return 0;
+	}
+
+}
